c43d1a249684831ed3becabd7ec7980f19a95ca5
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-08-28 16:12:31 -0400

    update

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 int main(){
     //imprimir o cabecalho do jogo
     printf("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
@@ -10,17 +12,28 @@ int main(){
 
     int chute;
 
-    printf("Qual é o seu chute? \n");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d \n", chute);
+    for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+        printf("Qual é o seu chute? \n");
+        printf("Tentativa %d de %d \n", i, NUMERO_DE_TENTATIVAS);
+        scanf("%d", &chute);
+        printf("Seu chute foi %d \n", chute);
 
-    if (chute == numSecreto){
-        printf("Parabéns, você acertou!!\n");
-    } else if (chute > numSecreto){
-        printf("Seu chute é maior que o numero secreto.\n");
-    } else if (chute < numSecreto){
-        printf("Seu chute é menor que o numero secreto.\n");
+        int acertou = (chute == numSecreto);
+
+        if (acertou){
+            printf("Parabéns, você acertou!!\n");
+            break;
+        } else {
+            int maior = chute > numSecreto;
+            if (maior){
+                printf("Seu chute é maior que o numero secreto.\n");
+            } else {
+                printf("Seu chute é menor que o numero secreto.\n");
+            }
+        } 
     }
+
+    printf("Fim de jogo, obrigado por jogar!!!\n");
     
     return 0;
 }
